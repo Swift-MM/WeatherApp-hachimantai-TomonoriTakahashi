f766b83a4bad17b27279882dcaac453b68ebe53d
Tomonori <tomonori0204.takahashi@gmail.com>
2016-11-16 00:28:54 +0900

    modify
    渡辺君のを見ながらoutlet collection等を使って共通化。正直あまり理解できてない。

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,24 +12,13 @@ import SwiftyJSON
 import SDWebImage
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var todayLabel: UILabel!
-    @IBOutlet weak var todayImage: UIImageView!
-    @IBOutlet weak var todayWeatherLabel: UILabel!
-    @IBOutlet weak var todayTemperatureLabel: UILabel!
+    @IBOutlet var weatherImageCollection: [UIImageView]!
+    @IBOutlet var weatherLabelCollection: [UILabel]!
     
-    @IBOutlet weak var tomorrowLabel: UILabel!
-    @IBOutlet weak var tomorrowImage: UIImageView!
-    @IBOutlet weak var tomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var tomorrowTemperatureLabel: UILabel!
-    
-    @IBOutlet weak var afterTomorrowLabel: UILabel!
-    @IBOutlet weak var afterTomorrowImage: UIImageView!
-    @IBOutlet weak var afterTomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var afterTomorrowTemperatureLabel: UILabel!
-    
+    let weatherJsonUrl:String = "http://weather.livedoor.com/forecast/webservice/json/v1?city=030010"
     
     
     // 気温のラベル用テキストを生成します。
@@ -53,15 +42,31 @@ class ViewController: UIViewController {
         
         return resultText
     }
-
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Alamofireを利用して通信を行います。
-        Alamofire.request("http://weather.livedoor.com/forecast/webservice/json/v1?city=030010").responseJSON { (response: DataResponse<Any>) in
-            
-            if response.result.isFailure {
+        Alamofire.request(weatherJsonUrl).responseJSON { (response: DataResponse<Any>) in self.getWeather(response)}
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // 閉じるボタンのみのアラートを表示します。
+    func simpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "閉じる", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func getWeather(_ response: DataResponse<Any>){
+          if response.result.isFailure {
                 self.simpleAlert(title: "通信エラー", message: "通信に失敗しました")
                 return
             }
@@ -69,6 +74,8 @@ class ViewController: UIViewController {
             // "guard let 変数 〜 else" で変数の中身がnilの場合のみの処理が書けます。
             // ただし最後に必ずreturnで関数を終了させなければいけません。
             // 変数は以後の関数内でも利用できます。
+            
+            
             guard let val = response.result.value as? [String: Any] else {
                 self.simpleAlert(title: "通信エラー", message: "通信結果がJSONではありませんでした")
                 return
@@ -81,62 +88,22 @@ class ViewController: UIViewController {
             if let title = json["title"].string {
                 self.titleLabel.text = title
             }
-            
-            // 天気の情報
+        
+            //天気の情報
             if let forecasts = json["forecasts"].array {
-                
-                // 今日の天気
-                let todayWeather = forecasts[0]
-                
-                self.todayLabel.text = todayWeather["dateLabel"].stringValue
-                if let imgUrl = todayWeather["image"]["url"].string {
-                    self.todayImage.sd_setImage(with: URL(string: imgUrl))
-                }
-                self.todayWeatherLabel.text = todayWeather["telop"].stringValue
-                self.todayTemperatureLabel.text = self.generateTemperatureText(todayWeather["temperature"])
-                
-                
-                
-                // 明日の天気
-                let tomorrowWeather = forecasts[1]
-                
-                self.tomorrowLabel.text = tomorrowWeather["dateLabel"].stringValue
-                if let imgUrl = tomorrowWeather["image"]["url"].string {
-                    self.tomorrowImage.sd_setImage(with: URL(string: imgUrl))
-                }
-                self.tomorrowWeatherLabel.text = tomorrowWeather["telop"].stringValue
-                self.tomorrowTemperatureLabel.text = self.generateTemperatureText(tomorrowWeather["temperature"])
-
-                
-                
-                
-                // 明後日の天気
-                if forecasts.count >= 3 {
-                    let afterTomorrowWeather = forecasts[2]
-                
-                    self.afterTomorrowLabel.text = afterTomorrowWeather["dateLabel"].stringValue
-                    if let imgUrl = afterTomorrowWeather["image"]["url"].string {
-                        self.afterTomorrowImage.sd_setImage(with: URL(string: imgUrl))
+                for var i:Int in 0..<forecasts.count{
+                        let weatherInfo:JSON = forecasts[i]
+                        
+                        self.weatherLabelCollection[i].text = weatherInfo["dateLabel"].stringValue + ":" + weatherInfo["telop"].stringValue + " " + self.generateTemperatureText(weatherInfo["temperature"])
+                        if let imgUrl = weatherInfo["image"]["url"].string {
+                            self.weatherImageCollection[i].sd_setImage(with: URL(string: imgUrl))
+                        }
                     }
-                    self.afterTomorrowWeatherLabel.text = afterTomorrowWeather["telop"].stringValue
-                    self.afterTomorrowTemperatureLabel.text = self.generateTemperatureText(afterTomorrowWeather["temperature"])
-                }
+                
             }
-
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
     
-    // 閉じるボタンのみのアラートを表示します。
-    func simpleAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "閉じる", style: .cancel, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
 
-}
 
